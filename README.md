da37b88ac8f77704974f4918b5119e890254bb77
sunilsarpanch <sunilchhillar62@gmail.com>
2025-02-24 21:26:21 +0530

    handle delimiter cases

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Step 1: download node using below link :
 https://nodejs.org/en/download
 
-Step 2: `npm install -g typescript`
-Step 3: `npm install -g tsc`
-Step 4: `npm start`
+Step 2: `npm install -g typescript`  <br />
+Step 3: `npm install -g tsc`  <br />
+Step 4: `npm start`  <br />
--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,26 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
+    let delimiter = ",";
+    if (numbers.indexOf("//") === 0) {
+        delimiter = "";
+        for (let index = 2; index < numbers.length; index++) {
+            if (!numbers[index] || parseInt(numbers[index + 1])) {
+                break;
+            }
+            delimiter += numbers[index];
+        }
+        numbers = numbers.replace(`//${delimiter}`, "").trim();
+    }
     let sum = 0;
     let negativeNos = "";
-    const numArray = numbers.split(/[\n,]/);
+    let regex = new RegExp("[\n" + delimiter + "]");
+    const numArray = numbers.split(regex);
     for (let num of numArray) {
         if (num.trim() !== "") {
             const no = parseInt(num.trim(), 10);
             if (no < 0) {
-                negativeNos = negativeNos ? `{${negativeNos}, ${no}}` : `${no}`;
+                negativeNos = negativeNos ? `${negativeNos}, ${no}` : `${no}`;
             }
             if (negativeNos) {
                 continue;
@@ -27,5 +39,20 @@ console.log("sum case 1 : ", add(""));
 console.log("sum case 2 : ", add("1"));
 console.log("sum case 3 : ", add("1,5"));
 console.log("sum case 4 : ", add("1,4,6,4"));
-console.log("sum is : ", add("1\n2,3"));
-console.log("sum is : ", add("-1;4"));
+console.log("sum case 5 : ", add("1\n2,3"));
+console.log("sum case 6 : ", add("//;\n1;2")); // handle delimiter ;
+console.log("sum case 7 : ", add("\n1,4"));
+console.log("sum case 8 : ", add("//&&\n1&&4")); // handle delimiter &&
+// throw error and handle using try catch
+try {
+    add("//;\n-1;2");
+}
+catch (err) {
+    console.log("sum error case 1 : ", err.message);
+}
+try {
+    add("-1, -5, 4");
+}
+catch (err) {
+    console.log("sum error case 1 : ", err.message);
+}
