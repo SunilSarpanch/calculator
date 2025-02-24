16eea018255b1b65e01206433dd41a04e4c77de5
sunilsarpanch <sunilchhillar62@gmail.com>
2025-02-24 21:33:59 +0530

    added few test scenarios

--- src/index.ts
+++ src/index.ts
@@ -49,9 +49,11 @@ console.log("sum case 2 : ", add("1"));
 console.log("sum case 3 : ", add("1,5"));
 console.log("sum case 4 : ", add("1,4,6,4"));
 console.log("sum case 5 : ", add("1\n2,3"));
-console.log("sum case 6 : ", add("//;\n1;2")); // handle delimiter ;
-console.log("sum case 7 : ", add("\n1,4"));
-console.log("sum case 8 : ", add("//&&\n1&&4")); // handle delimiter &&
+console.log("sum case 6 : ", add("1\n2\n,\n3"));
+console.log("sum case 7 : ", add("//;\n1;2")); // handle delimiter ;
+console.log("sum case 8 : ", add("\n1,4"));
+console.log("sum case 9 : ", add("//&&\n1&&4")); // handle delimiter &&
+console.log("sum case 10 : ", add("//&#&\n1&#&5")); // handle delimiter &#&
 
 // throw error and handle using try catch
 try {
